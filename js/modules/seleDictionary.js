346a75dbfe324a7090b7f7a3ad0e040df4e171a1
sergeyif1 <sergeyif1@gmail.com>
2025-07-21 19:48:59 +0300

    fix the seleDictionary.js

--- js/modules/seleDictionary.js
+++ js/modules/seleDictionary.js
@@ -3,21 +3,21 @@ const seleDictionary = document.getElementById("select2");
 let requestAdd;
 
 if (seleDictionary.value === "English dictionary") {
-  requestAdd = "../dbEn.json";
+  requestAdd = "/dbEn.json";
 }
 
 seleDictionary.addEventListener("change", function () {
   if (seleDictionary.value === "Deutsch Wörterbuch") {
-    requestAdd = "../dbDeu.json";
+    requestAdd = "/dbDeu.json";
   }
   if (seleDictionary.value === "English dictionary") {
-    requestAdd = "../dbEn.json";
+    requestAdd = "/dbEn.json";
   }
   if (seleDictionary.value === "Griechικό λεξικό") {
-    requestAdd = "../dbGr.json";
+    requestAdd = "/dbGr.json";
   }
   if (seleDictionary.value === "Polskie słownictwo") {
-    requestAdd = "../dbPl.json";
+    requestAdd = "/dbPl.json";
   }
 });
 
