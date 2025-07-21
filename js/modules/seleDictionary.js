c1ced5fd5cd7449a4d0cd80837e8c3c516e75f1e
sergeyif1 <sergeyif1@gmail.com>
2025-07-21 20:03:21 +0300

    #2

--- js/modules/seleDictionary.js
+++ js/modules/seleDictionary.js
@@ -3,21 +3,22 @@ const seleDictionary = document.getElementById("select2");
 let requestAdd;
 
 if (seleDictionary.value === "English dictionary") {
-  requestAdd = "../blob/main/dbEn.json";
+  requestAdd =
+    "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbEn.json";
 }
 
 seleDictionary.addEventListener("change", function () {
   if (seleDictionary.value === "Deutsch Wörterbuch") {
-    requestAdd = "../blob/main/dbDeu.json";
-  }
-  if (seleDictionary.value === "English dictionary") {
-    requestAdd = "../blob/main/dbEn.json";
+    requestAdd =
+      "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbDeu.json";
   }
   if (seleDictionary.value === "Griechικό λεξικό") {
-    requestAdd = "../blob/main/dbGr.json";
+    requestAdd =
+      "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbGr.json";
   }
   if (seleDictionary.value === "Polskie słownictwo") {
-    requestAdd = "../blob/main/dbPl.json";
+    requestAdd =
+      "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbPl.json";
   }
 });
 
