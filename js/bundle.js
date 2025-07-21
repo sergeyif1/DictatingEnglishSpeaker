2e35665d965e4ba63d8f0be2c3b43c1b9937f35b
sergeyif1 <sergeyif1@gmail.com>
2025-07-22 01:10:50 +0300

    #####5

--- js/bundle.js
+++ js/bundle.js
@@ -612,19 +612,19 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   getRequestAdd: () => (/* binding */ getRequestAdd)
 /* harmony export */ });
 var seleDictionary = document.getElementById("select2");
-var requestAdd;
-if (seleDictionary.value === "English dictionary") {
-  requestAdd = "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbEn.json";
-}
+var requestAdd = null;
 seleDictionary.addEventListener("change", function () {
-  if (seleDictionary.value === "Deutsch Wörterbuch") {
+  var value = seleDictionary.value;
+  if (value === "English dictionary") {
+    requestAdd = "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbEn.json";
+  } else if (value === "Deutsch Wörterbuch") {
     requestAdd = "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbDeu.json";
-  }
-  if (seleDictionary.value === "Griechικό λεξικό") {
+  } else if (value === "Griechικό λεξικό") {
     requestAdd = "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbGr.json";
-  }
-  if (seleDictionary.value === "Polskie słownictwo") {
+  } else if (value === "Polskie słownictwo") {
     requestAdd = "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbPl.json";
+  } else {
+    requestAdd = null;
   }
 });
 function getRequestAdd() {
--- js/modules/seleDictionary.js
+++ js/modules/seleDictionary.js
@@ -1,24 +1,24 @@
 const seleDictionary = document.getElementById("select2");
 
-let requestAdd;
-
-if (seleDictionary.value === "English dictionary") {
-  requestAdd =
-    "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbEn.json";
-}
+let requestAdd = null;
 
 seleDictionary.addEventListener("change", function () {
-  if (seleDictionary.value === "Deutsch Wörterbuch") {
+  const value = seleDictionary.value;
+
+  if (value === "English dictionary") {
+    requestAdd =
+      "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbEn.json";
+  } else if (value === "Deutsch Wörterbuch") {
     requestAdd =
       "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbDeu.json";
-  }
-  if (seleDictionary.value === "Griechικό λεξικό") {
+  } else if (value === "Griechικό λεξικό") {
     requestAdd =
       "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbGr.json";
-  }
-  if (seleDictionary.value === "Polskie słownictwo") {
+  } else if (value === "Polskie słownictwo") {
     requestAdd =
       "https://raw.githubusercontent.com/sergeyif1/DictatingEnglishSpeaker/main/dbPl.json";
+  } else {
+    requestAdd = null;
   }
 });
 
